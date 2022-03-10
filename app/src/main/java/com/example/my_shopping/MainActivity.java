52b2cdb626e2f02fb80f882461d0c492aa18b9fb
bxi4 <muhammedaahaj@gmail.com>
2022-03-10 22:06:28 +0300

    new commit

--- app/src/main/java/com/example/my_shopping/MainActivity.java
+++ app/src/main/java/com/example/my_shopping/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.my_shopping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bo = findViewById(R.id.button);
+        bo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
